977bfc6281273f53f10a8ff8c685197feda30e1d
Vippul Pandit <vippul.pandit@gmail.com>
2018-10-17 14:07:38 -0700

    Added XRef to do project

---
+++ src/main/java/com/orange/data/XRef.java
@@ -0,0 +1,40 @@
+package com.orange.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.orange.ct.aws.conf.AppModuleConfiguration;
+
+public class XRef {
+	public enum XRefType {
+		ICD10,
+		LABTEST,
+		LOINC,
+		MEDICAL_MACRO,
+		MEDICATION, 
+		CLINICAL_LAB_TEST,
+		
+	}
+	public double relevancy;
+	public List<CodeDO> codeXRefList;
+	private List<String> keyXRefList;
+	public XRefType type;
+	public XRef() {
+		codeXRefList = new ArrayList<>();
+		keyXRefList = new ArrayList<>();
+	}
+	public String toString() {
+		String value = "";
+		if(keyXRefList!=null && keyXRefList.size()>0)
+			value = keyXRefList.stream().reduce((t,u)->t.replace("\"","")+"|"+u.replace("\"","")).get();
+		return "{\"type\":\""+type.name()+"\", \"values\": \""+value.replace("\"","")+"\"}";
+	}
+	public List<String> getKeyXRefList(){
+		return this.keyXRefList;
+	}
+
+	
+
+}
